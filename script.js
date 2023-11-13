87b19bbbfd3129af74f4017841468f6e5bd929d9
Yoann Bettinelli <yoann@bunkerpalace.com>
2023-11-13 19:14:32 +0100

    intro ok

--- script.js
+++ script.js
@@ -1,72 +1,107 @@
 gsap.registerPlugin(ScrollTrigger) 
 
-const container = document.querySelector('.main-wrapper')
-const allSections = document.querySelectorAll('section')
-const wrapperSections = document.querySelectorAll('.main-wrapper section')
+const intro = document.querySelector('.intro')
+let sections = gsap.utils.toArray(".wrapper section");
 
 
 
-// TIMELINE sections 1 et 2
+// INTRO
 const timeline = gsap.timeline()
-
 timeline
-    .to(container, {
-        ease: 'none',
-        duration: 1,
-        // xPercent: -100 * (wrapperSections.length - 1 ), 
-        x: () => -(container.scrollWidth - document.documentElement.clientWidth) + 'px', // Déplace le container vers le haut
-    }) 
-    .to('.side-bar', {
-        left: 0,
-        opacity: 1,
-        scrollTrigger: {
-            trigger: '.side-bar',
-            start: 'center+=2500 center',
-            scrub: 2.5
-        }
-    })
-    .to({}, { duration: 1 / (allSections.length + 1)})
+    .to('.right', { xPercent: 100, duration: 0.5 }) 
+    .to('.title', { yPercent: -100 }, "<")
+    .to('.name', { y: '-100vh' }, "<")
+    .to ('.intro', { background: 'transparent', backgroundImage: "url('images/background-paper.jpg')", backgroundSize: 'contain', backgroundRepeat: 'no-repeat', opacity: 1, duration: 0.4 }, '<')
+    .to('.chapter-1', { left: 'calc(100vw - 270px)', zIndex: 10 }, "<")
+    .to('.chapter-2', { left: 'calc(100vw - 180px)', zIndex: 10}, "<")
+    .to('.chapter-3', { left: 'calc(100vw - 90px)', zIndex: 10}, "<")
 
 ScrollTrigger.create({
     animation: timeline,
-    trigger: container,
+    trigger: intro,
     scrub: 1,
     pin: true,
     start: 'top top',
-    end: `${container.scrollWidth}` // Fin scroll horizontal (Totalité des sections : 2 x 100vw)
 })
 
 
-gsap.from('.row li', {
-    y: 200,
+// SECTION 1 après intro
+gsap.from('.section-1 .block', {
+    y: 500, 
     opacity: 0,
-    ease: 'none',
     duration: 2,
-    delay: 1,
-    stagger: {
-        amount: 1   
-    },
     scrollTrigger: {
-        trigger: '.row li',
-        toggleActions: 'play pause reverse reverse'
-    }
-})
-
-
-gsap.from('.row-2', {
-    height: '0%',
-    duration: 1,
-    delay: 0.5,
-    scrollTrigger: {
-        trigger: '.section-3',
-        start: '40% center',
-        toggleActions: 'play pause reverse reverse'
+        trigger: '.section-1',
+        start: 'top 80%',
+        end: 'bottom top',
+        toggleActions: 'play reverse resume reverse'
     }
 })
 
 
 
+// SCROLL HORIZONTAL
+let scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".wrapper",
+      pin: true,
+      scrub: 0.1,
+      start: 'top top',
+      end: '+=3000'
+    }
+})
 
 
-/* * * * * NOTES * * * * */
-// container.scrollWidth => width de section-1 (100vw) + section-2(100vw) en pixel
+const timelineSection1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".section-1",
+        containerAnimation: scrollTween,
+        start: "center 40%",
+        // markers: true,
+        toggleActions: "play none none reverse",
+        id: "1"
+    }
+})
+
+timelineSection1.to('.chapter-3 .banner', { width: '50px', markers: true, duration: 0.5})
+timelineSection1.to('.chapter-3 .banner .vertical-text', { opacity: 0 }, '<0.2')
+timelineSection1.to('.chapter-3', { left: 'calc(100vw - 50px)' }, '<')
+
+timelineSection1.to('.chapter-2 .banner', { width: '50px', markers: true, duration: 0.5})
+timelineSection1.to('.chapter-2 .banner .vertical-text', { opacity: 0 }, '<0.2')
+timelineSection1.to('.chapter-2', { left: 'calc(100vw - 100px)' }, '<')
+
+timelineSection1.to('.chapter-1 .banner', { width: '50px', markers: true, duration: 0.5})
+timelineSection1.to('.chapter-1 .banner .vertical-text', { opacity: 0 }, '<0.2')
+timelineSection1.to('.chapter-1', { left: 'calc(100vw - 150px)' }, '<')
+
+timelineSection1.to('.brand', { left: 'calc(100vw - 500px'})
+
+
+
+
+
+
+// gsap.to(".chapter-1 .banner", {
+//     duration: 1,
+//     width: '50px',
+//     ease: "elastic",
+//     scrollTrigger: {
+//       trigger: ".section-1",
+//       containerAnimation: scrollTween,
+//       start: "center 40%",
+//       markers: true,
+//       toggleActions: "play none none reset",
+//       id: "1"
+//     }
+// });
+  
+
+
+
+
+  
+
+
