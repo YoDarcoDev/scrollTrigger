57e9a6c613c057c465e89a99ff9ee3700d73087d
Yoann Bettinelli <yoann@bunkerpalace.com>
2023-11-17 17:46:20 +0100

    chapter-2

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,23 +1,70 @@
-const notes = document.querySelectorAll('sup')
+const numbers = document.querySelectorAll('sup')
+const blockNotes = document.querySelector('.notes')
+let currentId = 0
 
 // Charger le fichier JSON
 const notesFile = fetch('../../files/notes.json')
     .then(response => {
         return response.json()
     })
-    .then(data => { console.log(data)})
+    .then(data => { 
 
-notes.forEach(note => {
-    note.addEventListener('click', () => {
-        const id = note.dataset.id 
-        
+        if (numbers) {
+            numbers.forEach(number => {
+                number.addEventListener('click', () => {
 
-        Object.entries(notesFile).forEach(([key], [value]) => {
-            console.log(key)
-        });
+                    const id = number.dataset.id
+                    let text = document.querySelector('.notes .text')
 
-     
+                    if (!blockNotes.classList.contains('active') && currentId != id) {
+                        text.innerHTML = `<strong>${id}</strong> - ${data.notes[id].text}`
+                        blockNotes.classList.add('active')
+                        currentId = number.dataset.id 
+                    }
+                    else if (blockNotes.classList.contains('active') && currentId == id) {
+                        blockNotes.classList.remove('active')
+                        currentId = 0
+                    } 
+                    else if (blockNotes.classList.contains('active') && currentId != id) {
+                        text.innerHTML = `<strong>${id}</strong> - ${data.notes[id].text}`
+                        currentId = number.dataset.id 
+                    }
+                })
+            })
+        }    
     })
+
+
+document.querySelector('.notes #close').addEventListener('click', () => {
+    blockNotes.classList.remove('active')
+    currentId = 0
 })
 
 
+
+
+
+
+
+
+// SWIPER BOOKS
+const swiper = new Swiper('.swiper-books', {
+    slidesPerView: 1,
+    spaceBetween: 80,
+    drag: true,
+    loop: true,
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 80,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 80,
+        },
+        1280: {
+            slidesPerView: 4,
+            spaceBetween: 80,
+        },
+    },
+});
