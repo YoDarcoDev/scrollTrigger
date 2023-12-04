0268e04ee41ee22d7d8a31041d6be278cb06a27f
Yoann Bettinelli <yoann@bunkerpalace.com>
2023-12-04 18:28:04 +0100

    chapter 1 pinned section

--- src/js/gsap-chapter-1.js
+++ src/js/gsap-chapter-1.js
@@ -1,4 +1,42 @@
 gsap.registerPlugin(ScrollTrigger) 
+const intro = document.getElementById('intro');
+const chapter1 = document.querySelector('.chapter-1')
+
+// TODO : Check si on est dans le chapitre 1
+// if (chapter1 !== null) {
+//     disableScroll()
+// }
+
+/* * * * * HEADER * * * * */
+// document.addEventListener('click', () => {
+//     intro.scrollIntoView({ behavior: 'smooth',});
+    
+//     gsap.to('#header', {
+//         opacity: 0,
+//         scrollTrigger: {
+//             trigger: "#header",
+//             scrub: 1,
+//             start: "top top" 
+//         }
+//     })
+
+//     enableScroll()
+// })
+
+
+// function disableScroll () {
+//     document.body.style.overflow = 'hidden';
+// }
+
+// const enableScroll = () => {
+//     document.body.style.overflow = 'unset';
+// }
+/* * * * * HEADER * * * * */
+
+
+
+
+
 
 
 /* * * * * INTERVIEW * * * * */
@@ -9,18 +47,17 @@ gsap.from('#interview-text', {
         trigger: "#interview",
         start: "top 70%",
         end: "top 40%",
-        markers: true
+        // markers: true
     }
 })
 
 gsap.from('#interview-video', {
-    y: 200,
+    y: 500,
     scrollTrigger: {
         scrub: 1,
         trigger: "#interview",
-        start: "top 70%",
-        end: "top 40%",
-
+        start: "top 40%",
+        end: "+=100",
         // markers: true
     }
 })
@@ -28,19 +65,25 @@ gsap.from('#interview-video', {
 
 
 
+
+
+
 /* * * * * CITATION * * * * */
 gsap.from('#citation', {
-    y: 200,
+    y: 300,
     opacity: 0,
+    scale: 0.5,
     scrollTrigger: {
         trigger: "#citation",
-        start: "top 60%",
-        end: "top 40%",
+        start: "top 90%",
+        end: "top 60%",
         scrub: 1,
         // markers: true
     }
 })
 /* * * * * CITATION * * * * */
+
+
 
 
 
@@ -71,16 +114,57 @@ gsap.from('#comic-strip-img', {
 
 
 
-/* * * * * SECTION 1 * * * * */
 
+/* * * * * SECTION 1 PINNED * * * * */
 ScrollTrigger.create({
     trigger: "#section-1",
     start: "top top", 
-    end: "+=5000",
     scrub: 1,
     pin: "#img-background",
+    end: 'bottom -=' + document.querySelector('#section-1').offsetHeight,
 });
+/* * * * * SECTION 1 PINNED * * * * */
 
 
 
-  
+
+
+/* * * * * SECTION 2 : JOURNAL KLEPELKRICH * * * * */
+ScrollTrigger.create({
+    trigger: "#section-2",
+    start: "top top",
+    end: '+=' + (document.querySelector('#block-section-2').offsetHeight - document.querySelector('#img-klepelkrich').offsetHeight),
+    pin: "#text",
+    pinSpacing: false,
+})
+
+gsap.from('#books-klepelkrich', {
+    y: 400,
+    opacity:0,
+    scrollTrigger: {
+        trigger: "#section-2",
+        scrub: 1,
+        start: "top top",
+        end: '+=200',
+        markers: true
+    }
+})
+
+/* * * * * SECTION 2 : JOURNAL KLEPELKRICH * * * * */
+
+
+
+
+
+/* * * * * IMAGE FULL TRAUSCH RTL * * * * */
+gsap.from('#img-rtl img', {
+    scale: 0.7,
+    scrollTrigger: {
+        trigger: "#img-rtl",
+        start: "top center",
+        end: "center 60%",
+        scrub: 1,
+        // markers: true
+    }
+})
+/* * * * * IMAGE FULL TRAUSCH RTL * * * * */
